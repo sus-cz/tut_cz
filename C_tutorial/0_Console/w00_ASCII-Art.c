8bfc7111ae1fdab0db6fd64999a549d7662f72e3
sus-cz <terabit_cz@protonmail.com>
2026-05-14 07:20:23 -0400

    0_Console

---
+++ C_tutorial/0_Console/w00_ASCII-Art.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+*	Jednoduchy nuz v ASCII
+*/
+
+int main(){
+	printf(")==[;;;;;;;> \n");
+	return 0;
+}
---
+++ C_tutorial/0_Console/w01_ASCII-Art.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*	Jednoduchy kruh/diamant v ASCII
+*/
+
+int main(){
+	printf("  -   \n");
+	printf(" / \\  \n");
+	printf("|   |   \n");
+	printf(" \\ /  \n");
+	printf("  -    \n");
+	return 0;
+}
